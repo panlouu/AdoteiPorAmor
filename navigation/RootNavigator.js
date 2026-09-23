8a44789b4354370c2a3efaca78a24f1c17b0ba1d
panlouu <pab10hrdeoliveira4@gmail.com>
2026-09-23 09:25:31 -0300

    Update RootNavigator.js

--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import BottomTabNavigator from './BottomTabNavigator';
+import StackNavigator from './StackNavigator';
+import RegistrationScreen from '../screens/RegistrationScreen';
+
+
+const RootStack = createStackNavigator();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        <RootStack.Screen name="MainTabs" component={BottomTabNavigator} />
+
+        <RootStack.Screen name="Lista" component={StackNavigator} />
+
+        <RootStack.Screen
+          name="Cadastro"
+          component={RegistrationScreen}
+          options={{ headerShown: true, title: 'Cadastrar Novo Pet 🐾' }}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+}
