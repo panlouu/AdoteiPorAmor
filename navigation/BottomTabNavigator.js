de4dfe760698fca97de1749b6a1f4b50ec4eff42
panlouu <pab10hrdeoliveira4@gmail.com>
2026-09-23 09:25:16 -0300

    Update BottomTabNavigator.js

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,1 +1,39 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'; // Importando ícones nativos do Expo
 
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import StackNavigator from './StackNavigator';
+import DetailScreen from '../screens/DetailScreen';
+import ListScreen from '../screens/ListScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Início') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Perfil') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+          else if (route.name === 'Pets') {
+            iconName = focused ? 'paw' : 'paw-outline'
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#FF6B6B',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Início" component={HomeScreen} />
+      <Tab.Screen name="Pets" component={ListScreen} />
+      <Tab.Screen name="Perfil" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
